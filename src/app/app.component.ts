8aa6d7eb10f9bbd245718b1fb38453ccd908f16e
Nir Oren <nir.oren@here.com>
2019-01-07 16:15:33 +0200

    stage 2 - loop with large change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-change-detection';
+  arr1 = [];
+  constructor() {
+    for (let index = 0; index < 10000; index++) {
+      this.arr1.push(index);
+    }
+  }
 }
--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-b',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./b.component.scss']
 })
 export class BComponent implements OnInit {
+
+  @Input()
+  input1: number;
 
   constructor() { }
 
